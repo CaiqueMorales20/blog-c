5ae9503ad7106213300daec5c0c497e1cf2fcaed
Caique <caiquemorales20@gmail.com>
2023-08-21 01:00:49 -0300

    :rocket: Next & ESLint

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+// Functional Component
+export default function Home() {
+  return (
+    <div className="bg-primary flex h-[100vh] w-full items-center justify-center">
+      <h1 className="text-base text-white">Blog C</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#263238',
+        secondary: '#B0BEC5',
+        accent: '#FF9800',
+      },
+      fontSize: {
+        sm: 'clamp(0.8rem, 0.17vw + 0.76rem, 0.89rem);',
+        base: 'clamp(1rem, 0.34vw + 0.91rem, 1.19rem)',
+        medium: 'clamp(1.25rem, 0.61vw + 1.1rem, 1.58rem)',
+        lg: 'clamp(1.56rem, 1vw + 1.31rem, 2.11rem)',
+        xl: ['clamp(1.95rem, 1.56vw + 1.56rem, 2.81rem)', '1.3'],
+        '2xl': 'clamp(2.44rem, 2.38vw + 1.85rem, 3.75rem)',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
